7385213039bf3c38bfb923fa5de4e8f409bbb43b
joschrew <>
2023-02-24 22:04:37 +0100

    Use separate module for my own changes

--- init.lua
+++ init.lua
@@ -493,36 +493,7 @@ cmp.setup {
   },
 }
 
--- my own additions start here
-
-vim.keymap.set('n', '<Home>', function()
-    local _,c = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.cmd('normal! ^')
-    local _,c1 = unpack(vim.api.nvim_win_get_cursor(0))
-    if c == c1 then
-        vim.cmd('normal! 0')
-    end
-end, { desc = 'jump to start of line or first char in line' })
-
-vim.keymap.set('n', '<C-S>', ':w<CR>', { silent = true, desc = 'Save with ctrl + s in normal mode' })
-vim.keymap.set('i', '<C-S>', '<Esc>:w<CR>', { silent = true, desc = 'Save with ctrl + s in insert mode' })
-vim.keymap.set('n', '<leader>y', '"+y', { desc = 'copy to clipboard' })
-vim.keymap.set('n', '<leader>p', '"+p', { desc = 'paste from clipboard' })
-
--- remove trailing whitespace
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*" },
-    callback = function()
-      local pos = vim.api.nvim_win_get_cursor(0)
-      vim.cmd([[%s/\s\+$//e]])
-      vim.api.nvim_win_set_cursor(0, pos)
-    end
-})
-
--- search with leader g and f in addition to leader sg and sf
-vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep 2' })
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles 2' })
-
+require('my-init')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
